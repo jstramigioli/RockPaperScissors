5eaa05a839a6212aadb5f0041d1c8088e7c3b49e
jstramigioli <julianstramigioli@gmail.com>
2023-03-19 23:46:55 -0300

    Added function playRound

--- script.js
+++ script.js
@@ -2,13 +2,43 @@
 function getComputerChoice() {
 
     switch (Math.floor(Math.random() * 3)) {
-        case 0: return 'Rock';
+        case 0: return 'rock';
             break;
-        case 1: return 'Paper';
+        case 1: return 'paper';
             break;
-        case 2: return 'Scissors';
+        case 2: return 'scissors';
             break;
-        default: return 'No valid number'
+        default: return 'getComputerChoice Failed'
     }
 }
-console.log(getComputerChoice())
+
+// Create a function that plays a single round of Rock Paper Scissors
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    switch(true) {
+        case (playerSelection === computerSelection) : return 'It\'s a TIE!';
+        break;
+        case (playerSelection === 'rock' && computerSelection === 'scissors') : return 'You win, rock beats scissors';
+        break;
+        case (playerSelection === 'rock' && computerSelection === 'paper') : return 'You lose, paper wraps rock';
+        break;
+        case (playerSelection === 'paper' && computerSelection === 'scissors') : return 'You lose, scissors cut paper';
+        break;
+        case (playerSelection === 'paper' && computerSelection === 'rock') : return 'You win, paper wraps rock';
+        break;
+        case (playerSelection === 'scissors' && computerSelection === 'paper') : return 'You win, scissors cut paper';
+        break;
+        case (playerSelection === 'scissors' && computerSelection === 'rock') : return 'You lose, rock smashes scissors';
+        break;
+        default: return "something went wrong with playRound"
+    }
+
+}
+
+// Set playerSelection
+const playerSelection = "Rock";
+
+// Set computerSelection
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))

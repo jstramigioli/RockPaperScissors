0fe19280062fc513684f4c46d2a96347b36676fb
jstramigioli <julianstramigioli@gmail.com>
2023-04-11 21:50:26 -0300

    Added basic UI

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
-// Create a function called game, wich makes a 5 round game that keeps score and reports a winner
-function game() {
-    let computerScore = 0
-    let playerScore = 0
-    for (let i = 0; playerScore < 3 && computerScore < 3;) {
-        // Set computerSelection
+let computerScore = 0;
+let playerScore = 0;
+let roundResult; 
+let resultMessage;
+
+const btnRock = document.createElement('button');
+btnRock.textContent = 'Rock';
+btnRock.classList.add('game-btn');
+
+const btnPaper = document.createElement('button');
+btnPaper.textContent = 'Paper';
+btnPaper.classList.add('game-btn');
+
+const btnScissor = document.createElement('button');
+btnScissor.textContent = 'Scissors';
+btnScissor.classList.add('game-btn');
+
+const btnContainer = document.querySelector('#btnContainer');
+
+const btnPlayAgain = document.createElement('button');
+btnPlayAgain.textContent = 'Play again?';
+
+const txtRoundResult = document.createElement('p');
+txtRoundResult.classList.add('txtResult');
+
+
+const txtCurrentScore = document.createElement('p')
+txtCurrentScore.classList.add('currentScore');
+
+
+let playRoundListener = function(e) {if 
+    (e.target.classList.contains('game-btn')) 
+    playRound(e)}
+
+function getRoundResult(playerSelection, playerScore, computerScore) {
     const computerSelection = getComputerChoice();
-    // Set playerSelection
-    let playerSelection = prompt('Select your weapon');
-    // Play Round
-       let roundResult = playRound(playerSelection, computerSelection, playerScore, computerScore);
-    // Update scores
-    playerScore = roundResult.playerScore;
-    computerScore = roundResult.computerScore;
-    // Log score
-        console.log(roundResult.resultMessage)
-        console.log('Player score: '+playerScore)
-        console.log('Computer score: '+computerScore)
-    }
-    playerScore>computerScore ? 
-    console.log("You win the game!") :
-    console.log("You lost the game")
-}
-
-// Create function getComputerChoice that will randomly return either 'Rock', 'Paper' or 'Scissors'
-function getComputerChoice() {
-
-    switch (Math.floor(Math.random() * 3)) {
-        case 0: return 'rock';
-            break;
-        case 1: return 'paper';
-            break;
-        case 2: return 'scissors';
-            break;
-        default: return 'getComputerChoice Failed'
-    }
-}
-
-// Create a function that plays a single round of Rock Paper Scissors
-
-function playRound(playerSelection, computerSelection, playerScore, computerScore) {
-    playerSelection = playerSelection.toLowerCase();
-    let resultMessage;
     switch(true) {
         case (playerSelection === computerSelection) : 
             resultMessage = 'It\'s a TIE!';
@@ -70,11 +63,87 @@ function playRound(playerSelection, computerSelection, playerScore, computerScor
             resultMessage = 'You lose, rock smashes scissors';
             break;
         default: 
-            return "something went wrong with playRound";
+            return "something went wrong with getRoundResult";
     }
+    txtRoundResult.textContent = resultMessage;
+    txtCurrentScore.textContent = playerScore+' - '+computerScore;
     return { playerScore, computerScore, resultMessage };
 }
 
+
+// Add event listener to buttons and play round with corresponding input
+function playWhenPressed(playerScore, computerScore) {
+    btnContainer.addEventListener('click', playRoundListener)
+}
+
+function playRound(e) {
+        if(e.target && e.target.tagName === 'BUTTON') {
+        roundResult = getRoundResult(e.target.textContent.toLowerCase(), playerScore, computerScore);
+        playerScore = roundResult.playerScore;
+        computerScore = roundResult.computerScore;
+        console.log(roundResult.resultMessage)
+        console.log('Player score: '+playerScore)
+        console.log('Computer score: '+computerScore)
+        if (playerScore === 5) {
+            txtRoundResult.textContent = 'You win the game!';
+            stopGame()
+        }
+        else if (computerScore === 5) {
+            txtRoundResult.textContent = 'You lost the game!';
+            stopGame()
+        }
+    }
+}
+
+
+// Create function getComputerChoice that will randomly return either 'Rock', 'Paper' or 'Scissors'
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0: return 'rock';
+            break;
+        case 1: return 'paper';
+            break;
+        case 2: return 'scissors';
+            break;
+        default: return 'getComputerChoice Failed'
+    }
+}
+
+function initGame() {
+    computerScore = 0;
+    playerScore = 0;
+    btnContainer.appendChild(txtCurrentScore);
+    btnContainer.appendChild(txtRoundResult);
+    btnContainer.appendChild(btnRock);
+    btnContainer.appendChild(btnPaper);
+    btnContainer.appendChild(btnScissor);
+    txtCurrentScore.textContent = playerScore+' - '+computerScore;
+}
+
+function stopGame() {
+    btnContainer.removeEventListener('click', playRoundListener)
+    btnContainer.removeChild(btnRock);
+    btnContainer.removeChild(btnPaper);
+    btnContainer.removeChild(btnScissor);
+    btnContainer.removeChild(txtRoundResult);
+    btnContainer.appendChild(btnPlayAgain);
+    btnPlayAgain.addEventListener('click', playAgain);
+    txtRoundResult.textContent = 'Choose your Weapon';
+}
+
+function playAgain() {
+    btnContainer.removeChild(btnPlayAgain);
+    game()
+}
+
+function game() {
+    initGame();
+    playWhenPressed(playerScore, computerScore);
+    }
+    
 game()
 
-nextRound()
+
+
+
+
